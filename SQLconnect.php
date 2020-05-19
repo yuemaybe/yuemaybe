12f69ef22f4ea6ae51a526b2758d28fcc4c9352b
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-19 11:13:16 +0800

    Update SQLconnect.php

--- SQLconnect.php
+++ SQLconnect.php
@@ -1,5 +1,15 @@
 <?php
 
-	echo "123444444";
+	//SQL連線(include用)
+	$mysqli = new mysqli('192.168.1.13', 'gameuser', '' , 'game');
+
+	if($mysqli -> connect_error)
+	{
+		echo "資料庫連線失敗<br>";
+	}
+	else
+	{
+		echo "成功";
+	}
 
 ?>
