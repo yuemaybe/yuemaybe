33d6cda2a912c57a93b2b04d2f75efce37653cda
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-29 01:19:18 +0800

    Update test.php

--- front/test.php
+++ front/test.php
@@ -9,22 +9,23 @@
 	</head>
 	<body bgcolor="#ECFFFF"> 
 		<nav class="yoyoman">
-		<a href="../index.html" rel="yoyoman">關於本人</a> 
-		<a href="../front/Portfolio.html" rel="yoyoman">關於作品</a> 
-		<a href="google.php" rel="yoyoman">測試登入</a> 
-	</nav>
+			<a href="../index.html" rel="yoyoman">關於本人</a> 
+			<a href="../front/Portfolio.html" rel="yoyoman">關於作品</a> 
+			<a href="google.php" rel="yoyoman">測試登入</a> 
+		</nav>
 
-	<div class="wiki-inner">
+		<div class="wiki-inner">
 
-		<?php
-			session_start();
-			require_once '../vendor/autoload.php';
-			require_once '../google/gclient.php';
-			
-			echo 'wtf?';
-		?>
+			<?php
+				session_start();
+				require_once '../vendor/autoload.php';
+				require_once '../google/gclient.php';
+				
+				echo "<h1 align='center'>請先登入（Google）</h1>";
+				echo "<form align='center' action='google2.php'><button>登入</button></form>";
+			?>
 
-	</div>
+		</div>
 
 	</body>
 </html>
