6ef1d2f664a77ea07f55d520a261f493e3f465c5
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-28 13:49:28 +0800

    google

--- google/google.php
+++ google/google.php
@@ -1,33 +1,49 @@
+<html>
+<head>
+
+<meta charset="UTF-8">
+<title>HiImYG</title>
+<link rel="stylesheet" href="../front/CSS.css">
+
+</head>
+<body bgcolor="#ECFFFF"> 
+<nav class="yoyoman">
+<a href="../index.html" rel="yoyoman">關於本人</a> 
+<a href="../front/Portfolio.html" rel="yoyoman">關於作品</a> 
+<a href="google.php" rel="yoyoman">小遊戲啦</a> 
+</nav>
+
+<div class="wiki-inner">
+
 <?php
-session_start();
-require_once 'vendor/autoload.php';
-
-$client = new Google_Client();
-
-//導入google下載的json資料
-$client -> setAuthConfig('client_secret.json');
-
-//取得授權後拿到的資訊
-$client -> addScope('https://www.googleapis.com/auth/userinfo.email'); //email
-$client -> addScope('https://www.googleapis.com/auth/userinfo.profile'); //個人資料
-
-
-if(isset($_SESSION['access_token']) && $_SESSION['access_token'])
-{
-	$client -> setAccessToken($_SESSION['access_token']);
+	session_start();
+	require_once 'vendor/autoload.php';
+	require_once 'gclient.php';
 	
-	$service = new Google_Service_Oauth2($client);
-	$user_info = $service -> userinfo -> get();
-
-	$open_id = $user_info -> id;
-	$email = $user_info -> email;
-	$name = $user_info -> name;
 	
-	header('Location: https://hiimyg.herokuapp.com/game.php?');
-}
-else
-{
-	header('Location: https://hiimyg.herokuapp.com/glogin.php?');
-}
+	if(!isset($_SESSION['access_token']))
+	{
+		echo "<p align='center'>請先登入（Google）</p>";
+		echo "<form align='center' action='google.php'><button>登入</button></form>";
+	}else
+	{
+		$client -> setAccessToken($_SESSION['access_token']);
+		$service = new Google_Service_Oauth2($client);
+		$user_info = $service -> userinfo -> get();
+		$open_id = $user_info -> id;
+		$email = $user_info -> email;
+		$name = $user_info -> name;
+		
+		echo "<p align='center'>登入成功！</p>";
+		echo "<p align='center'>歡迎 " . $name . " 玩家登入！</p>";
+		echo "<form align='center' action='https://hiimyg.herokuapp.com/gamestart.php?'><button>進入遊戲</button></form>";
+		echo "<form align='center' action='https://hiimyg.herokuapp.com/glogout.php?'><button>登出</button></form>";
+	}
+
 
 ?>
+
+</div>
+
+</body>
+</html>
