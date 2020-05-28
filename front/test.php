5813c76409b6999e013b3bc109b0266f48f231f5
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-29 01:22:20 +0800

    Update test.php

--- front/test.php
+++ front/test.php
@@ -19,7 +19,6 @@
 			<?php
 				session_start();
 				require_once '../vendor/autoload.php';
-				require_once '../google/gclient.php';
 				
 				echo "<h1 align='center'>請先登入（Google）</h1>";
 				echo "<form align='center' action='google2.php'><button>登入</button></form>";
