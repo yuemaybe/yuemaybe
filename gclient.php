7f50a7aa7640c45a356db03950fd417098d21981
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-28 12:32:08 +0800

    Update gclient.php

--- gclient.php
+++ gclient.php
@@ -5,5 +5,4 @@ $client = new Google_Client();
 $client -> setAuthConfig('client_secret.json');
 $client -> addScope('https://www.googleapis.com/auth/userinfo.email'); //email
 $client -> addScope('https://www.googleapis.com/auth/userinfo.profile'); //個人資料
-
 ?>
