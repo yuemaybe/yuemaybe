4f56765a4f29afddf16bc2553d6bc68c76fac4e4
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-28 14:12:28 +0800

    Update google.php

--- google/google.php
+++ google/google.php
@@ -1,50 +1,50 @@
+<!DOCTYPE html>
 <html>
-<head>
+	<head>
 
-<meta charset="UTF-8">
-<title>HiImYG</title>
-<link rel="stylesheet" href="../front/CSS.css">
+		<meta charset="UTF-8">
+		<title>HiImYG</title>
+		<link rel="stylesheet" href="../front/CSS.css">
 
-</head>
-<body bgcolor="#ECFFFF"> 
-<nav class="yoyoman">
-<a href="../index.html" rel="yoyoman">關於本人</a> 
-<a href="../front/Portfolio.html" rel="yoyoman">關於作品</a> 
-<a href="google.php" rel="yoyoman">小遊戲啦</a> 
-</nav>
+	</head>
+	<body bgcolor="#ECFFFF"> 
+		<nav class="yoyoman">
+		<a href="../index.html" rel="yoyoman">關於本人</a> 
+		<a href="../front/Portfolio.html" rel="yoyoman">關於作品</a> 
+		<a href="google.php" rel="yoyoman">小遊戲啦</a> 
+	</nav>
 
-<div class="wiki-inner">
+	<div class="wiki-inner">
 
-<h1 align='center'>請先登入</h1>
+		<h1 align='center'>請先登入</h1>
 
-<?php
-	session_start();
-	require_once '../vendor/autoload.php';
-	require_once 'gclient.php';
-	
-	
-	if(!isset($_SESSION['access_token']))
-	{
-		echo "<p align='center'>請先登入（Google）</p>";
-		echo "<form align='center' action='google2.php'><button>登入</button></form>";
-	}else
-	{
-		$client -> setAccessToken($_SESSION['access_token']);
-		$service = new Google_Service_Oauth2($client);
-		$user_info = $service -> userinfo -> get();
-		$open_id = $user_info -> id;
-		$email = $user_info -> email;
-		$name = $user_info -> name;
-		
-		echo "<p align='center'>登入成功！</p>";
-		echo "<p align='center'>歡迎 " . $name . " 玩家登入！</p>";
-		echo "<form align='center' action='https://hiimyg.herokuapp.com/google/glogout.php?'><button>登出</button></form>";
-	}
+		<?php
+			session_start();
+			require_once '../vendor/autoload.php';
+			require_once 'gclient.php';
+			
+			if(!isset($_SESSION['access_token']))
+			{
+				echo "<p align='center'>請先登入（Google）</p>";
+				echo "<form align='center' action='google2.php'><button>登入</button></form>";
+			}
+			
+			else
+			{
+				$client -> setAccessToken($_SESSION['access_token']);
+				$service = new Google_Service_Oauth2($client);
+				$user_info = $service -> userinfo -> get();
+				$open_id = $user_info -> id;
+				$email = $user_info -> email;
+				$name = $user_info -> name;
+				
+				echo "<p align='center'>登入成功！</p>";
+				echo "<p align='center'>歡迎 " . $name . " 玩家登入！</p>";
+				echo "<form align='center' action='https://hiimyg.herokuapp.com/google/glogout.php?'><button>登出</button></form>";
+			}
+		?>
 
+	</div>
 
-?>
-
-</div>
-
-</body>
+	</body>
 </html>
