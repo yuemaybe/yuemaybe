8c0b8dd0a314b9889c0f32f62e00659ca6c4b318
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-28 19:19:39 +0800

    Update FBcallback.php

--- facebook/FBcallback.php
+++ facebook/FBcallback.php
@@ -53,15 +53,15 @@
 	// var_dump($accessToken -> getValue());
 
 	//OAuth2.0會幫忙管理token
-	// $oauth = $fb -> getOAuth2Client();
+	$oauth = $fb -> getOAuth2Client();
 
-	// // //從debug_token取得metadata
-	// $tokenMetadata = $oauth -> debugToken($accessToken);
-	// // echo '<h3>Metadata</h3>';
-	// // var_dump($tokenMetadata);
+	// //從debug_token取得metadata
+	$tokenMetadata = $oauth -> debugToken($accessToken);
+	// echo '<h3>Metadata</h3>';
+	// var_dump($tokenMetadata);
 
-	// $tokenMetadata -> validateAppId($config['599570660765510']);
-	// $tokenMetadata -> validateExpiration();
+	$tokenMetadata -> validateAppId($config['599570660765510']);
+	$tokenMetadata -> validateExpiration();
 
 	// if(!$access -> isLongLived())
 	// {
@@ -80,6 +80,6 @@
 	// }
 
 	// $_SESSION['fb_access_token'] = $accessToken;
-	// // header('Location: https://hiimyg.herokuapp.com/facebook/FBtest.php?');
+	// header('Location: https://hiimyg.herokuapp.com/facebook/FBtest.php?');
 
 ?>
