25960e1e6896cf3ea23545202cf30b037715751f
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-29 00:30:08 +0800

    Update FBtest.php

--- facebook/FBtest.php
+++ facebook/FBtest.php
@@ -1,14 +1,30 @@
 <?php
 	session_start();
 	require_once '../vendor/autoload.php';
+
+	$fb = new Facebook\Facebook(
+		[
+			'app_id' => '599570660765510',
+			'app_secret' => 'f33b813f2a800fe6a0d4996360167df1',
+			'default_graph_version' => 'v2.10',
+		]
+	);
 	
-	if(isset($_SESSION['fb_access_token']))
-	{
-		echo 'welcome FB';
+	try{
+		$response = $fb -> get('/me?field=id,name' , $_SESSION['fb_access_token']);
 	}
-	else
+	catch(Facebook\Exception\ResponseException $e) 
 	{
-		echo 'failed!';
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} 
+	catch(Facebook\Exception\SDKException $e) 
+	{
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
 	}
+
+	$user = $response -> getGraphUser();
+	echo 'hello ' . $user['name'];
 
 ?>
