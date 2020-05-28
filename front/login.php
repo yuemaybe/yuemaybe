a0fbdcd07126cc07562ccab0d541b195e4eb3eae
yuemaybe <58838655+yuemaybe@users.noreply.github.com>
2020-05-29 01:05:49 +0800

    Update login.php

--- front/login.php
+++ front/login.php
@@ -30,49 +30,7 @@
 
 			else
 			{
-				if(isset($_SESSION['access_token']))
-				{
-					$client -> setAccessToken($_SESSION['access_token']);
-					$service = new Google_Service_Oauth2($client);
-					$user_info = $service -> userinfo -> get();
-					$name = $user_info -> name;
-					
-					echo "<p align='center'>您使用的是Google登入</p>";
-					echo "<p align='center'>登入成功！</p>";
-					echo "<p align='center'>歡迎 " . $name . " 登入！</p>";
-					echo "<form align='center' action='https://hiimyg.herokuapp.com/google/glogout.php?'><button>登出</button></form>";
-				}
-				elseif(isset($_SESSION['fb_access_token']))
-				{
-					$fb = new Facebook\Facebook(
-						[
-							'app_id' => '599570660765510',
-							'app_secret' => 'f33b813f2a800fe6a0d4996360167df1',
-							'default_graph_version' => 'v3.1',
-						]
-					);
-					
-					try{
-						$response = $fb -> get('/me?field=id,name' , $_SESSION['fb_access_token']);
-					}
-					catch(Facebook\Exception\ResponseException $e) 
-					{
-						echo 'Graph returned an error: ' . $e->getMessage();
-						exit;
-					} 
-					catch(Facebook\Exception\SDKException $e) 
-					{
-						echo 'Facebook SDK returned an error: ' . $e->getMessage();
-						exit;
-					}
-					
-					$user = $response -> getGraphUser();
-					
-					echo "<p align='center'>您使用的是FaceBook登入</p>";
-					echo "<p align='center'>登入成功！</p>";
-					echo "<p align='center'>歡迎 " . $user['name'] . " 登入！</p>";
-					echo "<form align='center' action='https://hiimyg.herokuapp.com/facebook/FBlogout.php?'><button>登出</button></form>";
-				}
+				echo 'hhh';
 			}
 		?>
 
